3083c0727c8352ed9e11aeb17550f447a5c1af20
Anastasia_Poleshuk <71152591+Lai-LMU@users.noreply.github.com>
2021-03-12 15:46:37 +0300

    feat: add 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (array == null || array == undefined || array.length == 0){
+	  return 0;
+  } 
+  
+  if(array == null || array == undefined || array.length == 0){  
+    return 0; 
+	}else {
+    let m= Math.min.apply(null, array);
+	  return m;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null || array == undefined || array.length == 0){
+	  return 0;
+	}else {
+	  let m= Math.max.apply(null, array);
+	  return m;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+	if (array == null || array == undefined || array.length == 0){
+	  return 0;
+  }else if (array.length==0){
+    return 0; 
+	}else {
+	  let sum=0;
+	  for(let i=0; i<array.length; i++){
+			sum += array[i];	
+	   }
+	  let a = sum/array.length;
+	  return a;
+	}
 }
